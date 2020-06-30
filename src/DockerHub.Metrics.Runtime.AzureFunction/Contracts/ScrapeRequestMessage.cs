ccce1bd03af442b9fd839e5768c5461d36a744f1
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-06-30 18:23:51 +0200

    Use queue trigger rather than timer

---
+++ src/DockerHub.Metrics.Runtime.AzureFunction/Contracts/ScrapeRequestMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace DockerHub.Metrics.Runtime.AzureFunction.Contracts
+{
+    public class ScrapeRequestMessage
+    {
+        public string RepoName { get; set; }
+        public string ImageName { get; set; }
+    }
+}
