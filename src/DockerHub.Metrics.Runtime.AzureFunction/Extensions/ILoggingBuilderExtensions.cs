0997eb9672702352bd28b7568e1ed87f41bf9b1c
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-07-03 10:13:08 +0200

    Reduce amount of providers to only what we want

---
+++ src/DockerHub.Metrics.Runtime.AzureFunction/Extensions/ILoggingBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using GuardNet;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DockerHub.Metrics.Runtime.AzureFunction.Extensions
+{
+    public static class ILoggingBuilderExtensions
+    {
+        public static ILoggingBuilder ClearProvidersExceptFunctionProviders(this ILoggingBuilder loggingBuilder)
+        {
+            Guard.NotNull(loggingBuilder, nameof(loggingBuilder));
+
+            // Kudos to katrash: https://stackoverflow.com/questions/45986517/remove-console-and-debug-loggers-in-asp-net-core-2-0-when-in-production-mode
+            foreach (ServiceDescriptor serviceDescriptor in loggingBuilder.Services)
+            {
+                if (serviceDescriptor.ServiceType == typeof(ILoggerProvider))
+                {
+                    if (serviceDescriptor.ImplementationType.FullName != "Microsoft.Azure.WebJobs.Script.Diagnostics.HostFileLoggerProvider"
+                        && serviceDescriptor.ImplementationType.FullName != "Microsoft.Azure.WebJobs.Script.Diagnostics.FunctionFileLoggerProvider")
+                    {
+                        loggingBuilder.Services.Remove(serviceDescriptor);
+                        break;
+                    }
+                }
+            }
+
+            return loggingBuilder;
+        }
+    }
+}
